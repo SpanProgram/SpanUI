a0561c50c0487ffd1ebc57e8fdbeeb92058a8ffb
Rafal Dworznik <rapiernik@gmail.com>
2018-11-04 18:28:22 +0100

    add aplicationt title + move to proper package name

--- src/pl/com/engineering/span/Main.java
+++ src/pl/com/engineering/span/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package pl.com.engineering.span;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Span Engineering Software");
+        primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
 
---
+++ src/pl/com/engineering/span/controller/Controller.java
@@ -0,0 +1,4 @@
+package pl.com.engineering.span.controller;
+
+public class Controller {
+}
